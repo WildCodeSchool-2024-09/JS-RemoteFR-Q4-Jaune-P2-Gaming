baf3a655f186a9a78a4b4f924985b5a1f61688d4
Mounir <izem.mounir@gmail.com>
2025-01-20 09:33:43 +0100

    clean dead code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -18,11 +18,6 @@ import NewGames from "./pages/NewGames/NewGames";
 import Quizz from "./pages/Quizz/Quizz";
 import Results from "./pages/Results/Results";
 
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import {
-//   getCentralContent,
-// } from "./services/requests.ts"
 import { getCalendriers, getGames, getNewGames } from "./services/requests.ts";
 
 /* ************************************************************************* */
